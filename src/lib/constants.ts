d1b7eb475118d89efc3040f872bae1cc5e53286b
Abdullahgz440 <99542952+Abdullahgz440@users.noreply.github.com>
2026-06-12 04:49:32 -0400

    Create constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,14 @@
+export const KANO_LGAS = [
+  "Ajingi", "Albasu", "Bagwai", "Bebeji", "Bichi", "Bunkure", "Dala", "Dambatta",
+  "Dawakin Kudu", "Dawakin Tofa", "Doguwa", "Fagge", "Gabasawa", "Garko", "Garun Mallam",
+  "Gaya", "Gezawa", "Gwale", "Gwarzo", "Kabo", "Kano Municipal", "Karaye", "Kibiya",
+  "Kiru", "Kumbotso", "Kunchi", "Kura", "Madobi", "Makoda", "Minjibir", "Nasarawa",
+  "Rano", "Rimin Gado", "Rogo", "Shanono", "Sumaila", "Takai", "Tarauni", "Tofa",
+  "Tsanyawa", "Tudun Wada", "Ungogo", "Warawa", "Wudil"
+];
+
+export const EVENT_TYPES = [
+  "Wedding", "Walima", "Naming Ceremony", "Conference", "Seminar",
+  "Training", "Exhibition", "Trade Fair", "Corporate Event",
+  "Birthday", "Religious Event"
+];
